c1c1f21aff83f20ed58e1f951e3fc421b68c7f66
craizytech <craizytech@gmail.com>
2024-01-06 23:23:11 +0300

    if_else_loops_and_functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number *= -1
-        last_digit = (number % 10) * -1
-        return last_digit
-    else:
-        last_digit = number % 10
-        return last_digit
+    last_digit = number % 10
+    return last_digit
