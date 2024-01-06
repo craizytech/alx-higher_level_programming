7d4422df58e76fcf7e6dc1652edb39c1f3c5650c
craizytech <craizytech@gmail.com>
2024-01-06 23:12:08 +0300

    if_else_loops_and_functions

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        num = ord(c)
+        if 93 <= num <= 122:
+            num -=32
+        print("{}".format(chr(num)), end="")
+    print()
