c5937f706bbf729abc389400ec28db1e2e5ae583
craizytech <craizytech@gmail.com>
2024-01-08 21:17:34 +0300

    fixed typo in docstring

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    """A function that replaces an elemrt of a list at a specific position."""
+    """A function that replaces an elemet of a list at a specific position."""
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     my_list[idx] = element
