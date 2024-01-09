6e00892a8e9c5ba7a294d48969eb7c9d70e17d29
craizytech <craizytech@gmail.com>
2024-01-09 14:29:33 +0300

    replacing an element in the list without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specific position."""
+    if idx < 0 or len(my_list) - 1:
+        return my_list[:]
+    new_list = mylist[:]
+    new_list[idx] = element
+    return new_list
