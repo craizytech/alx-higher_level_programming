af2e8a312d4e29a3b1c4a9211f7424a58362471e
craizytech <craizytech@gmail.com>
2024-01-09 16:38:57 +0300

    Fixed the code for some edge cases of input

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,6 +10,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         sum = tuple_a[0], 0
     elif len(tuple_a) == 0 and len(tuple_b) == 1:
         sum = 0, tuple_b[0]
+    elif len(tuple_a) == 0 and len(tuple_b) == 0:
+        sum = 0, 0
     elif len(tuple_a) == 0:
         return tuple_b
     elif len(tuple_b) == 0:
