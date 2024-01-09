c7c0ac9f9f1badf5cf48f811d67586d2243c17e4
craizytech <craizytech@gmail.com>
2024-01-09 20:07:36 +0300

    fixed code to handle an edge case if the list is empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """Finds the biggest integer of a list."""
+    if len(my_list) == 0:
+        return
     num = my_list[0]
     for x in my_list:
         if x > num:
