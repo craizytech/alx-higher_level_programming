fc2a29217a2994558033f6083a8aa0621c660dca
craizytech <craizytech@gmail.com>
2024-01-09 16:43:54 +0300

    modified the code to fit edge cases v2

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,6 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         sum = tuple_a[0] + tuple_b[0], tuple_a[1]
     elif len(tuple_a) == 1 and len(tuple_b) > 1:
         sum = tuple_a[0] + tuple_b[0], tuple_b[1]
+    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+        sum = tuple_a[0], tuple_b[0]
     elif len(tuple_a) == 1 and len(tuple_b) == 0:
         sum = tuple_a[0], 0
     elif len(tuple_a) == 0 and len(tuple_b) == 1:
