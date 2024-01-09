f5a2d39a7f75531d50aba4119dd0abccc4bb468b
craizytech <craizytech@gmail.com>
2024-01-09 17:45:54 +0300

    modified the code to a simpler, shorter optimized version

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) > 1 and len(tuple_b) > 1:
-        sum = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    elif len(tuple_a) > 1 and len(tuple_b) == 1:
-        sum = tuple_a[0] + tuple_b[0], tuple_a[1]
-    elif len(tuple_a) == 1 and len(tuple_b) > 1:
-        sum = tuple_a[0] + tuple_b[0], tuple_b[1]
-    elif len(tuple_a) > 1 and len(tuple_b) == 0:
-        sum = tuple_a[0], tuple_a[1]
-    elif len(tuple_a) == 0 and len(tuple_b) == 1:
-        sum = tuple_b[0], tuple_b[1]
-    elif len(tuple_a) == 1 and len(tuple_b) == 1:
-        sum = tuple_a[0], tuple_b[0]
-    elif len(tuple_a) == 1 and len(tuple_b) == 0:
-        sum = tuple_a[0], 0
-    elif len(tuple_a) == 0 and len(tuple_b) == 1:
-        sum = 0, tuple_b[0]
-    elif len(tuple_a) == 0 and len(tuple_b) == 0:
-        sum = 0, 0
-    elif len(tuple_a) == 0:
-        return tuple_b
-    elif len(tuple_b) == 0:
-        return tuple_a
-    return sum
+    """Adds two tuples."""
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
