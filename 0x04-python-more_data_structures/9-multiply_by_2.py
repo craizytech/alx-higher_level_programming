23dd33f2c0a1ad6ce5d43842de7c876eed0a6113
craizytech <craizytech@gmail.com>
2024-01-13 11:52:21 +0300

    pycodestyle error changes

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    """function that returns a new dictionary with all values multiplied by 2."""
+    """Returns a new dictionary with all values multiplied by 2."""
     new_dict = {}
     for i in a_dictionary:
         new_dict[i] = a_dictionary[i] * 2
