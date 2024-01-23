a81afb36555f67305dfb8aba4f08c981894a24c2
craizytech <craizytech@gmail.com>
2024-01-23 18:54:34 +0300

    error_handling

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,8 @@ def safe_print_integer(value):
     try:
         new_value = value / 0
     except ZeroDivisionError:
+        if value != int(value):
+            return False
         print("{:d}".format(value))
         return True
     except:
