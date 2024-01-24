c7c089c0538471d23a774add6866d945f052bb85
craizytech <craizytech@gmail.com>
2024-01-24 22:02:53 +0300

    error_handling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
-        print(f"Exception: {e}", file=sys.stderr)
-        return False
-    except ZeroDivisionError:
+    except (ValueError, ValueError) as e:
         print(f"Exception: {e}", file=sys.stderr)
         return False
