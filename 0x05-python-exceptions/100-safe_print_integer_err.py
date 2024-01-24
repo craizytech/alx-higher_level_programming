052d5f92780b1cacdd9d2fdfc186b8a715be76d0
craizytech <craizytech@gmail.com>
2024-01-24 21:57:46 +0300

    error_handling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,12 +3,8 @@ import sys
 def safe_print_integer_err(value):
     """Prints an integer."""
     try:
-        new_val = value / 1
         print("{:d}".format(value))
         return True
-    except TypeError as e:
-        print(f"Exception: {e}", file=sys.stderr)
-        return False
     except ValueError as e:
         print(f"Exception: {e}", file=sys.stderr)
         return False
