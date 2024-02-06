8f465c6a81600ace8d862b9783b2b5d98ba347a9
craizytech <craizytech@gmail.com>
2024-02-06 11:04:44 +0300

    documenting the class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""This module defines a class Square."""
 class Square:
+    """This is a Square class."""
     def __init__(self, size):
+        """The init method of the init class.
+
+        Args:
+            size : Describes the size of the square
+        """
         self.__size = size
