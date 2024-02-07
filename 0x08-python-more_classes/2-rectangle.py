7104dbfbece10a16831e73eadbc1e292ca1316d0
craizytech <craizytech@gmail.com>
2024-02-07 18:31:41 +0300

    more_classes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -87,4 +87,7 @@ class Rectangle:
         Args:
             returns the perimeter of the rectangle
         """
-        return 2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
