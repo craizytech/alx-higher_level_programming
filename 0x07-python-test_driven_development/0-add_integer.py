e25244bfd7d44f944824d64c912ce20c0087756e
craizytech <craizytech@gmail.com>
2024-02-16 00:40:14 +0300

    Test driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains a function that performs a sum and returns a value."""
+
+
+def add_integer(a, b=98):
+    """This methods takes 2 args sums them up and returns the value.
+
+    Args:
+        a (int or float): first argument
+        b (int or float): second argument(optional, default = 98)
+
+    Returns:
+        int or float: the sum of the two arguments
+    """
+    return a + b
