a1c04d9329f99646da1eef9f5690b0c2d9f4888e
craizytech <craizytech@gmail.com>
2024-02-17 15:06:58 +0300

    Test Driven Development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-# 5-text_indentation.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a text-indentation function."""
-
+"""This module defines a function that indents a string"""
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
+    """This function takes in a string and indents it based
+    on the characters '., ?, :' by adding two lines after each
+    of the characters
+    
     Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
-    """
-    if not isinstance(text, str):
+        text (str): The string to be indented"""
+
+    if isinstance(text, str):
+        text = text.strip()
+        sentences = []
+        sentence = ""
+        for char in text:
+            if char in ['.', '?', ':']:
+                sentence += char
+                sentences.append(sentence + '\n\n')
+                sentence = ""
+            else:
+                sentence += char
+
+        if sentence:
+            sentences.append(sentence)
+
+        indented_text = "".join(sentences)
+        print(indented_text, end="")
+    else:
         raise TypeError("text must be a string")
-
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
