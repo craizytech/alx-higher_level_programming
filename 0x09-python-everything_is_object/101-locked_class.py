a6cdbec4ffebe27a323f482d3d5fc35184f7e4d6
craizytech <craizytech@gmail.com>
2024-02-18 15:52:53 +0300

    Everything is an object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a locked class"""
+
+
+class LockedClass:
+    """This class prevents a user form dynamically creating new instance
+    attributes.
+    """
+
+    __slots__ = ('first_name')
+
+    def __init__(self, first_name):
+        self.first_name = first_name
