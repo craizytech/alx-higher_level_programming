43f3080844921b1fdcdca3c34d0b796282558419
craizytech <craizytech@gmail.com>
2024-02-22 11:44:43 +0300

    removed whitespaces

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,11 +4,11 @@
 
 def lookup(obj):
     """This method returns a list of available attributes of an object
-    
+ 
     Args:
         obj (object): The object whose attributes we need to obtain
-    
+
     Returns:
         list containing the attributes and methods of that object"""
-    
+
     return dir(obj)
