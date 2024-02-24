a8b9a736acf3cc999a93aa857f8b3af81546edc6
craizytech <craizytech@gmail.com>
2024-02-24 12:23:22 +0300

    Python Input Output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module writes a string to a text file."""
+
+
+def write_file(filename="", text=""):
+    """This module writes a string to a text file.
+    
+    Args:
+        filename (string): The name of the file
+        text (string): the text string of arguments
+        
+    Returns:
+        The number of characters written
+    """
+
+    with open(filename, mode='w', encoding="utf-8") as text_file:
+        num = text_file.write(text)
+    
+    return num
