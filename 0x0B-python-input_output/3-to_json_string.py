4a985199321c8efacda2bb6bdd2eb5cc34ee0733
craizytech <craizytech@gmail.com>
2024-02-24 12:47:41 +0300

    Python Input Output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module creates a json object"""
+
+
+import json
+
+def to_json_string(string):
+    """This method returns the JSON Representation of an object
+
+    Args:
+        string (str): string to be converted to json object
+
+    Returns:
+        json (object)
+    """
+
+    return json.loads(string)
