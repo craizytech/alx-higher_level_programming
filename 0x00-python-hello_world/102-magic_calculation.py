82ac1df15696a3c02779cfea7dca47d91d1a67d4
craizytech <craizytech@gmail.com>
2024-03-01 12:46:46 +0300

    added docs

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b):
-        return 98 + (a**b)
+def magic_calculation(a,b):
+    """This method adds 98 to the result of a pow b"""
+    return 98 + (a + b)
