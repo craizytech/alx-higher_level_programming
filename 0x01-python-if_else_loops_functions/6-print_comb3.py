c84cad0674a5e32eaf23ea4fb138996f765871b2
craizytech <craizytech@gmail.com>
2024-03-01 15:23:17 +0300

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
-for i in range(99):
-    
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            num = (i * 10) + j
+            if num < 89:
+                print("{:02d} ".format(num), end=",")
+            else:
+                print("{:02d}".format(num))
