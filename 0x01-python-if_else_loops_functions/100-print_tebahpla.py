a497e66a6711cecc32726b7caa7d23a7fd2fc500
craizytech <craizytech@gmail.com>
2024-03-01 21:04:18 +0300

    If, else and loops

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+flag = True
+for i in range(122:96:-1):
+    if flag:
+        flag = False
+    else:
+        flag = True
+        i -= 32
+    print("{}".format(chr(i)), end="")
