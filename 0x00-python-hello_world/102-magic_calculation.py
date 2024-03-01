2dc57f50be5f1d06147307282bc79192f406cb03
craizytech <craizytech@gmail.com>
2024-03-01 12:48:00 +0300

    here goes nothing

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-def magic_calculation(a,b):
-    """This method adds 98 to the result of a pow b"""
-    return 98 + (a + b)
+def magic_calculation(a, b):
+        return 98 + (a ** b)
