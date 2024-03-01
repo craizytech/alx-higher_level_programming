f59655ac5127301c6ccdb47c577928180a5554ad
craizytech <craizytech@gmail.com>
2024-03-01 12:59:07 +0300

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/7-islower.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def islower(c):
-    char = ord(c)
-    if 93 <= char <= 122:
-        return True
-    else:
-        return False
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        num = ord(c)
-        if 93 <= num <= 122:
-            num -= 32
-        print("{}".format(chr(num)), end="")
-    print()
