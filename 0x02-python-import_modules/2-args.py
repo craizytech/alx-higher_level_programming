c050ec1e3cf48eccee9143441cea1288ed4393e5
craizytech <craizytech@gmail.com>
2024-03-02 15:59:05 +0300

    Import and Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,14 @@ if __name__ == "__main__":
     arg_list = sys.argv
     arg_count = len(sys.argv) - 1
 
+    if arg_count == 0:
+        print("0 arguments.")
+        exit(1)
+    elif arg_count == 1:
+        print("1 argument:")
+        print(f"1: {arg_list[1]}")
+        exit(1)
+
     print(f"{arg_count} arguments:")
-    for i in range(arg_count):
-        print(f"{i}: {arg_list[i+1]}")
+    for i in range(1, arg_count):
+        print(f"{i}: {arg_list[i]}")
