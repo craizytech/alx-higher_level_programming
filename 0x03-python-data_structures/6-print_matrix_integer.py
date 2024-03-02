b2033f20c3b8227a9cced971b48e272da09dab2e
craizytech <craizytech@gmail.com>
2024-03-02 19:16:42 +0300

    Import and Modules

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
+            if num == row[len(row) - 1]:
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d}".format(num), end=" ")
         print()
