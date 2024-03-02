396d147ac9570343a1dbe1da96840af8f2ca9317
craizytech <craizytech@gmail.com>
2024-03-02 17:29:33 +0300

    Import and Modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    arg_list = sys.argv
+    arg_count = len(arg_list)
+
+    if arg_count != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if arg_list[2] not in ['+', '-', '/', '*']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(arg_list[1])
+    b = int(arg_list[3])
+
+    if arg_list[2] == "+":
+        print(f"{a} {arg_list[2]} {b} = {a + b}")
+    elif arg_list[2] == "-":
+        print(f"{a} {arg_list[2]} {b} = {a - b}")
+    elif arg_list[2] == "*":
+        print(f"{a} {arg_list[2]} {b} = {a * b}")
+    elif arg_list[2] == "/":
+        print(f"{a} {arg_list[2]} {b} = {a / b}")
