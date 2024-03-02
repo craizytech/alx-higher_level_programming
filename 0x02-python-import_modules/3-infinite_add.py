9c28560be53481de3ad9ffeaeffd51bf4b0d7f28
craizytech <craizytech@gmail.com>
2024-03-02 16:14:40 +0300

    Import and Modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    arg_list = sys.argv
+    arg_count = len(sys.argv)
+    sum = 0
+
+    for i in range(1, arg_count):
+        sum += int(arg_list[i])
+    
+    print(sum)
