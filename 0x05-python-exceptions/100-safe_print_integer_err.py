3e47801672fef656bdae7ffcb807da58e2e7b99c
craizytech <craizytech@gmail.com>
2024-03-06 22:02:16 +0300

    python_exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    """This function prints an integer."""
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as e:
+        print("Exception:", e, file=sys.stderr)
+        return False
