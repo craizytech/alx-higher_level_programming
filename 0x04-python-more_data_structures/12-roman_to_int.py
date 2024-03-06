2094207a16d630a59bf7918c57b63146234e5c98
craizytech <craizytech@gmail.com>
2024-03-06 14:32:50 +0300

    more_datastructures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    roman = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
     total = 0
     prev_value = 0
