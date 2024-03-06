544e7a668ee799808853119532603e14ff13540b
craizytech <craizytech@gmail.com>
2024-03-06 20:08:27 +0300

    python_exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     printed_elements = 0
-        try:
-            for i in range(x):
-                print(my_list[i], end="")
-                printed_elements += 1
-        except IndexError:
-            pass
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            printed_elements += 1
+    except IndexError:
+        pass
     print()
     return printed_elements
 
