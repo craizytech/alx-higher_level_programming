78559732f6e0ece77308b45f589fa9123162da9d
craizytech <craizytech@gmail.com>
2024-03-06 16:49:12 +0300

    more_datastructures

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,6 +3,7 @@
 def simple_delete(a_dictionary, key=""):
     """This function deletes a key in a dictionary."""
     if a_dictionary:
-        if key in list(a_dictionary.keys()):
-            del a_dictionary[key]
-        return a_dictionary
+        for a_key in a_dictionary.copy():
+            if a_key == key:
+                del a_dictionary[key]
+    return a_dictionary
