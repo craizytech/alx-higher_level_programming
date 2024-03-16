33de543fb9e09cdb3e8b103cb1437719d8d1eae8
craizytech <craizytech@gmail.com>
2024-03-16 20:29:12 +0300

    sql_introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,9 +7,9 @@
 -- Table first table
 -- Field name in first_table
 
-ALTER DATABASE `hbtn_0c_0`
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+-- ALTER DATABASE `hbtn_0c_0`
+-- CHARACTER SET utf8mb4
+-- COLLATE utf8mb4_unicode_ci;
 
 USE `hbtn_0c_0`;
 
@@ -17,7 +17,7 @@ ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE `first_table`
-MODIFY COLUMN `name` VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+-- ALTER TABLE `first_table`
+-- MODIFY COLUMN `name` VARCHAR(256)
+-- CHARACTER SET utf8mb4
+-- COLLATE utf8mb4_unicode_ci;
