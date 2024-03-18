9ef5d7448d2057dfde9dce6f39ef901f87561c9b
craizytech <craizytech@gmail.com>
2024-03-18 09:29:49 +0300

    sql_more_queries

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Creates the database hbtn_0d_usa and the table cities with some constraints
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	states_id INT FOREIGN KEY REFERENCES states(id),
+	name VARCHAR(256));
