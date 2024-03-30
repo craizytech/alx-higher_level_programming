07e9a41962cbe2f495b40f6597f453bbdeb0acc4
craizytech <craizytech@gmail.com>
2024-03-30 10:19:42 +0300

    python_classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is a square class implementation."""
+
+
+class Square:
+    """class contains the attributes methods that can be applied to a class."""
+    def __init__(self, size):
+        """This is the constructor class for the class Square object."""
+        self.__size = size
