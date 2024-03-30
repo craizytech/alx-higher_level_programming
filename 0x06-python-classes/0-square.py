ddda17ccb04fd19f9a89ac5d01539db6dc6ab3f3
craizytech <craizytech@gmail.com>
2024-03-30 10:12:08 +0300

    python_classes

--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a Class Square."""
-
-
-class Square:
-    """This class is used to define the properties of a square."""
-    pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a Class Square."""
-
-
-class Square:
-    """This class is used to define the properties of a square."""
-    def __init__(self, size):
-        self.__size = size
