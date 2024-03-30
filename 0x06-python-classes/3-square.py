05c1c37aa6ba765612e9aa183c8a3d8f9afa3ba9
craizytech <craizytech@gmail.com>
2024-03-30 10:42:40 +0300

    python_classes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is a square class implementation."""
+
+
+class Square:
+    """class contains the attributes methods that can be applied to a class."""
+    def __init__(self, size=0):
+        """This is the constructor class for the class Square object."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """This method returns the current area of the square."""
+        return self.__size ** 2
+
