1397977cc3aaf6eef9c627ababb772ec143e7ecc
craizytech <craizytech@gmail.com>
2024-04-01 17:37:03 +0300

    restored the code to the way it was previously

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -12,8 +12,5 @@ def is_kind_of_class(obj, a_class):
     Returns:
         Boolean
     """
-    #return isinstance(obj, a_class)
-    if issubclass(obj, a_class) or type(obj) is a_class:
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class)
+
