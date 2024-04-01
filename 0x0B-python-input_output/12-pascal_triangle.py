31ad76a66c96c44687b3c7b72574191cbfa25f46
craizytech <craizytech@gmail.com>
2024-04-01 13:08:42 +0300

    python_input_output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -15,6 +15,6 @@ def pascal_triangle(n):
         temp = [0] + res[-1] + [0]
         row = []
         for j in range(len(res[-1]) + 1):
-            row.append[temp[j] + temp[j + 1])
+            row.append(temp[j] + temp[j + 1])
         res.append(row)
     return res
