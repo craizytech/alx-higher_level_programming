1c68a24d1313fab56a55f7779500fd27a9ff7fe9
craizytech <craizytech@gmail.com>
2024-04-02 19:07:04 +0300

    python_inheritance

---
+++ 0x0A-python-inheritance/8-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module defines a class base Geometry."""
+
+
+class BaseGeometry:
+    """This class contains geometry implementations."""
+    def __init__(self, width, height):
+        """This is a constructor method."""
+        integer_validator(width)
+        integer_validator(height)
+
+        self.__width =  width
+        self.__height = height
+
+    def area(self):
+        """This method calculates the area."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This method validates an integer
+
+        Args:
+            name (string): this is the name of the value
+            value (int): this is the value of the integer
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value <= 0
+        """
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
