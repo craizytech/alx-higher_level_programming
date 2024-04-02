2a90f1cdcda7e77b8dacc333c97978a5e6ab7ff5
craizytech <craizytech@gmail.com>
2024-04-03 00:27:49 +0300

    python_everything_is_an_object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,7 +7,11 @@ class LockedClass:
     attributes.
     """
 
-    __slots__ = ('first_name',)
+    __slots__ = ()
 
     def __init__(self, first_name):
         self.first_name = first_name
+
+    def __setattr__(self, name, value):
+        if name == 'first_name':
+            self.__dict__[name] = value
