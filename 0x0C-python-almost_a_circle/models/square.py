02e0659943ab15570a8098c8a174b9e0c4793c1c
craizytech <craizytech@gmail.com>
2024-04-04 23:21:41 +0300

    python_almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is the Square module."""
+from . import rectangle
+
+class Square(rectangle.Rectangle):
+    """This is the Square class."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """This is the costructor class."""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """This is method prints the object representation."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+        )
