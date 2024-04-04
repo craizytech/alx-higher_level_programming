36ba2e1339db16e28bc5ee67318c5723e9f07ee3
craizytech <craizytech@gmail.com>
2024-04-04 21:29:22 +0300

    python_almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,11 @@ class Rectangle(base.Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height
         )
+
+    def update(self, *args):
+        """This method updates the Attributes of the rectangle class."""
+        self.id = args[0] if len(args) > 0 else self.id
+        self.width = args[1] if len(args) > 1 else self.width
+        self.height = args[2] if len(args) > 2 else self.height
+        self.x = args[3] if len(args) > 3 else self.x
+        self.y = args[4] if len(args) > 4 else self.y
