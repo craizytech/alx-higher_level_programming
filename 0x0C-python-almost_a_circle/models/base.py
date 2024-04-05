9cbfc1be98c0895702e2d6c59fbca0094059d1a4
craizytech <craizytech@gmail.com>
2024-04-05 23:52:00 +0300

    python_almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,20 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """This method saves the json obj rep of list_objs to a file
+
+        Args:
+            list_objs (object): list of instances who inherits of Base
+        """
+        filename = cls.__name__ + ".json"
+
+        with open(filename, "w", encoding="utf-8") as jsonfile:
+            if list_objs is None:
+                dict_list = []
+            else:
+                dict_list = [obj.to_dictionary() for obj in list_objs]
+            json_string = Base.to_json_string(dict_list)
+            jsonfile.write(json_string)
