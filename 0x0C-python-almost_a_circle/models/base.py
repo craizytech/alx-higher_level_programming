70742fcb277c3ff74b5d72cd5ea17fc761e114e4
craizytech <craizytech@gmail.com>
2024-04-06 00:00:46 +0300

    python_almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,6 +29,19 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """This method returns a list of dictionary object representations.
+
+        Args:
+            json_string (str): json object representation
+        Returns:
+            a python data structure
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """This method saves the json obj rep of list_objs to a file
