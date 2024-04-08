6551aa953786e46e099ce052ddf57487cb26fb43
craizytech <craizytech@gmail.com>
2024-04-09 00:20:32 +0300

    javascript_warmup

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg1 = Number(process.argv[2]);
+const arg2 = Number(process.argv[3]);
+
+function add (a, b) {
+  if (!isNaN(a) && !isNaN(b)) {
+    const result = a + b;
+    console.log(result);
+  } else {
+    console.log('NaN');
+  }
+}
+add(arg1, arg2);
