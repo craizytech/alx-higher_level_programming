4bd955dfadaacff9b5a960b2b6099fd09578ae20
craizytech <craizytech@gmail.com>
2024-04-11 02:10:09 +0300

    javascript warmup

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.logMe = function (item) {
-	if (typeof exports.logMe.counter === 'undefined') {
-		exports.logMe.counter = 0;
-	}
+  if (typeof exports.logMe.counter === 'undefined') {
+    exports.logMe.counter = 0;
+  }
 
-	console.log(exports.logMe.counter + ':' + item);
+  console.log(exports.logMe.counter + ':' + item);
 
-	exports.logMe.counter++;
+  exports.logMe.counter++;
 };
