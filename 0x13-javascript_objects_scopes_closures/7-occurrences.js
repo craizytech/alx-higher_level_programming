dd1611a50a677fc68051442b14b2038acedf476f
craizytech <craizytech@gmail.com>
2024-04-10 06:03:50 +0300

    javascript closures

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.forEach(function (value) {
+    if (value === searchElement) {
+      count += 1;
+      }
+  });
+  return count;
+}
