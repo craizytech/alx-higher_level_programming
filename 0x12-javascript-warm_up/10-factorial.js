afb831f66c6d9a07c9f4b0164807304c257fccb9
craizytech <craizytech@gmail.com>
2024-04-11 01:52:50 +0300

    javascript warmup

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 const arg1 = Number(process.argv[2]);
 
 function factorial (a) {
-  if (a === 0 || a === 1 || a === NaN) {
+  if (a === 0 || a === 1) {
     return 1;
   } else if (!isNaN(a) && a > 0) {
     return a * factorial(a - 1);
   } else {
-    console.log('1');
+    return 1;
   }
 }
 console.log(factorial(arg1));
