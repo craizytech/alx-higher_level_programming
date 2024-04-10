0c5dbf2d755c6cf9d0d63255c3a8313cb3daf732
craizytech <craizytech@gmail.com>
2024-04-10 06:20:11 +0300

    javascript closures

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.toReversed();
+  let new_list = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    new_list.push(list[i]);
+  }
+  return new_list;
 };
