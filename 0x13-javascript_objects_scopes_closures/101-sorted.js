d4f742a478c29023b9b0978d0bf9540275e6f6f8
craizytech <craizytech@gmail.com>
2024-04-11 02:33:07 +0300

    javascript warmup

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data');
+const newDict = {};
+for (const userId in dict) {
+  const occurences = dict[userId];
+  if (!newDict[occurences]) {
+    newDict[occurences] = [];
+  }
+  newDict[occurences].push(userId);
+}
+console.log(newDict);
