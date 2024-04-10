87de48125ac0ae1cab3237d9d0af836bd56dfc1a
craizytech <craizytech@gmail.com>
2024-04-10 05:53:22 +0300

    javascript closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        let line = '';
+        for (let j = 0; j < this.width; j++) {
+          line += c;
+        }
+        console.log(line);
+      }
+    }
+  }
+}
+
+module.exports = Square;
