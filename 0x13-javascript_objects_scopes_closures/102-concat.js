81c41cb53b87841621399f7127de44062e0284e8
craizytech <craizytech@gmail.com>
2024-04-11 02:57:42 +0300

    javascript warmup

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,13 +7,19 @@ const fs = require('fs');
 const [, , source1, source2, dest] = process.argv;
 
 // Read the contents of the first file
-fs.readFile(source1, 'utf8', (data1) => {
-	// read contents of 2nd souce file
-	fs.readFile(source2, 'utf8', (data2) => {
-		// concatenate the files contents
-		const concat = data1 + '\n' + data2;
+fs.readFile(source1, 'utf8', (err, data1) => {
+  if (err) {
+    process.exit(1);
+  }
+  // read contents of 2nd souce file
+  fs.readFile(source2, 'utf8', (err, data2) => {
+    if (err) {
+      process.exit(1);
+    }
+    // concatenate the files contents
+    const concat = data1 + data2;
 
-		// Write the concat to the dest
-		fs.writeFile(dest, concat, 'utf8', () => {});
-	});
+    // Write the concat to the dest
+    fs.writeFile(dest, concat, 'utf8', () => {});
+  });
 });
