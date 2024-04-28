76fdefd001143be07c2d7aec3fa47832d992899d
craizytech <craizytech@gmail.com>
2024-04-28 21:54:51 +0300

    Filter states by the the last argument to the script

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa and filter for states
+where the name matches the last argument provided """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        db = MySQLdb.connect(
+                host="localhost",
+                user=sys.argv[1],
+                password=sys.argv[2],
+                db=sys.argv[3],
+                port=3306)
+
+    cursor = db.cursor()
+
+    # fetch all the states from the database
+    cursor.execute("SELECT id, name FROM states ORDER BY id")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
