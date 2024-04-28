2b805ef80fbebab2e2ea69c3825290e2e8d2a3fa
craizytech <craizytech@gmail.com>
2024-04-28 21:10:42 +0300

    This script fetches data from a database and presents it

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa."""
+import MySQLdb;
+import sys
+
+if len(sys.argv) == 4:
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],\
+            password=sys.argv[2], db=sys.argv[3])
+
+    cursor = db.cursor()
+
+    # fetch all the states from the database
+    cursor.execute("SELECT id, name FROM states")
+    rows = cursor.fetchall()
+    
+    for row in rows:
+            print(row)
