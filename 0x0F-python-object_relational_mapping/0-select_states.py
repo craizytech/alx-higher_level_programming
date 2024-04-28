7bec36b33ab1a79b98e0b9c27b0852ff9afdb1b7
craizytech <craizytech@gmail.com>
2024-04-28 21:17:44 +0300

    Results must be sorted in ascending order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,12 @@ import sys
 
 if len(sys.argv) == 4:
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],\
-            password=sys.argv[2], db=sys.argv[3], port='3306')
+            password=sys.argv[2], db=sys.argv[3], port=3306)
 
     cursor = db.cursor()
 
     # fetch all the states from the database
-    cursor.execute("SELECT id, name FROM states")
+    cursor.execute("SELECT id, name FROM states ORDER BY id")
     rows = cursor.fetchall()
     
     for row in rows:
