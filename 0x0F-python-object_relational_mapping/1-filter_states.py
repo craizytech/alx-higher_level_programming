cd5102d7c6a09f92efba023eff7d317099d8dd83
craizytech <craizytech@gmail.com>
2024-04-28 21:39:49 +0300

    Filter states by the first letter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        db = MySQLdb.connect(
+                host="localhost",
+                user=sys.argv[1],
+                password=sys.argv[2],
+                db=sys.argv[3],
+                port=3306)
+
+    cursor = db.cursor()
+
+    # fetch all the states from the database
+    cursor.execute("SELECT id, name FROM states\
+            WHERE name LIKE 'N%' ORDER BY id")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
