63a771aaef6b5b68cd89eeeae50d368fc1f068c8
craizytech <craizytech@gmail.com>
2024-04-28 22:37:58 +0300

    Edited the previous file to remove potential mysql injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa and filter for states
+where the name matches the last argument provided This script should be 
+safe from mysql injections"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        db = MySQLdb.connect(
+                host="localhost",
+                user=sys.argv[1],
+                password=sys.argv[2],
+                db=sys.argv[3],
+                port=3306)
+
+    cursor = db.cursor()
+
+    # fetch all the states from the database
+    cursor.execute("SELECT id, name FROM states\
+                   WHERE name LIKE BINARY %s\
+                   ORDER BY id", (sys.argv[4]))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
