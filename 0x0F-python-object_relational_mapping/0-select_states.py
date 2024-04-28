09b49a5b63913b97c7714ca8f33e5a2b1a7d2217
craizytech <craizytech@gmail.com>
2024-04-28 21:27:57 +0300

    Fixing pycodestyle Errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa."""
-import MySQLdb;
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
-        db = MySQLdb.connect(host="localhost", user=sys.argv[1],\
-                password=sys.argv[2], db=sys.argv[3], port=3306)
+        db = MySQLdb.connect(
+                host="localhost",
+                user=sys.argv[1],
+                password=sys.argv[2],
+                db=sys.argv[3],
+                port=3306)
 
     cursor = db.cursor()
 
     # fetch all the states from the database
     cursor.execute("SELECT id, name FROM states ORDER BY id")
     rows = cursor.fetchall()
-    
+
     for row in rows:
-            print(row)
+        print(row)
