c547f594603da93bcc5863e62649c9e2b3fa190e
craizytech <craizytech@gmail.com>
2024-04-28 22:24:30 +0300

    Filter states by the the last argument to the script v4

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,8 +16,9 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # fetch all the states from the database
-    query = "SELECT id, name FROM states\
-            WHERE name LIKE '{}' ORDER BY states.id ASC".format(sys.argv[4])
+    query = """SELECT id, name FROM states
+            WHERE name LIKE BINARY '{}'
+            ORDER BY states.id ASC""".format(sys.argv[4])
     cursor.execute(query)
     rows = cursor.fetchall()
 
