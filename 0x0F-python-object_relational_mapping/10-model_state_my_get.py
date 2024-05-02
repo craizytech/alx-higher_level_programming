64d8880ba2cccac3901ffe059291e83d277381a3
craizytech <craizytech@gmail.com>
2024-05-02 06:16:33 +0300

    10-model_state_my_get
    Prints the state object with name passed as an argument

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Lists all state objects that have a specific name"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    # Create the database connection
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # Create a Session object and bind to the engine
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Query the database
+    state = session.query(State).order_by(State.id).first()
+
+    # Print the states according to the requirements
+    if state:
+        if sys.argv[4] == state.name:
+            print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    # Close the session out
+    session.close()
