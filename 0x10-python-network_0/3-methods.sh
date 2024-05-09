0920d624d51b9afc59cca0edc58501bcbba9313f
craizytech <craizytech@gmail.com>
2024-05-10 00:57:26 +0300

    made some corrections on the file 2 and 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script returns the request methods that a server can accept
-curl -X OPTIONS -s -I "$1" | grep -i Allow | awk '{ print $2 }'
+curl -sI "$1" | grep "Allow" | awk '{ print $2 }'
