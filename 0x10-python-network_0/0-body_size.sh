5e42fded6a33bdb7eacf1418508815e2a306abe8
craizytech <craizytech@gmail.com>
2024-05-09 23:37:02 +0300

    corrected the requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # This script takes in a url,sends a request to that url and displays the 
 # size of the body in its response
-# curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
-curl -s "$1" | wc -c
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
