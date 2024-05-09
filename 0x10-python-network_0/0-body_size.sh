cd90f42ad7f48f962a7a13d4f1a879bc3f209998
craizytech <craizytech@gmail.com>
2024-05-09 23:16:26 +0300

    Python network
    using curl to get the size of the server content

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This script takes in a url,sends a request to that url and displays the 
+# size of the body in its response
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
