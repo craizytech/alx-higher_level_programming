32226fcafda90ff0a7eabdcbdf8cf10a9263ded5
craizytech <craizytech@gmail.com>
2024-05-10 00:36:05 +0300

    using grep and awk to get the response

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script returns the request methods that a server can accept
-curl -sIX "$1" | grep -i Allow | awk '{ print $2 }'
+curl -X OPTIONS -s -I "$1" | grep -i Allow | awk '{ print $2 }'
