bee5d687c457dba9976b920e5b797d7160251c74
craizytech <craizytech@gmail.com>
2024-05-14 11:36:21 +0300

    This script takes in a URL sends a POST Message with an email and a subject

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This bash script takes in a URL, sends a GET request to the URL and displays the body of the response
+# Get the response body of URLS with a 200 response
 curl -sL "$1"
