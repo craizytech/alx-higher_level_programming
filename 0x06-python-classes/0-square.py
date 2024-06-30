6beef06b8229f88faf54b6af5cf5f9da9c0dcb61
craizytech <craizytech@gmail.com>
2024-06-30 17:01:23 +0300

    redoing the tasks in python classes project

--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""This is a square class implementation."""
-
-
-class Square:
-    """class contains the attributes methods that can be applied to a class."""
-    pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This is a square class implementation."""
-
-
-class Square:
-    """class contains the attributes methods that can be applied to a class."""
-    def __init__(self, size):
-        """This is the constructor class for the class Square object."""
-        self.__size = size
