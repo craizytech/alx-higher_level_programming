4c0a6fb14d17b3bee801d5ebac4a32cf9f5cb921
craizytech <craizytech@gmail.com>
2024-06-30 17:09:27 +0300

    created a private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates the square class"""
+
+
+class Square:
+    """This is the Square class."""
+    
+    def __init__(self):
+        """This is the constructor method"""
+        __size = None
