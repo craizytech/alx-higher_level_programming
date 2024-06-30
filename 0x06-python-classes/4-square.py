d68602c7bd4468ab0b48eea72cf146f21c9ba678
craizytech <craizytech@gmail.com>
2024-06-30 19:27:10 +0300

    added the getters and setter for the private size attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module creates the square class"""
+
+
+class Square:
+    """This is the Square class."""
+
+    def __init__(self, size=0):
+        """This is the constructor method"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """This is the size attribute getter."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """This method returns the area of a square"""
+        return self.__size * self.__size
