baabcc29de6209c9eb6854a00e9a8e0c18958301
craizytech <craizytech@gmail.com>
2024-06-30 19:02:02 +0300

    added type validators for the size instance attribute

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module creates the square class"""
+
+
+class Square:
+    """This is the Square class."""
+
+    def __init__(self, size=0):
+        """This is the constructor method"""
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
