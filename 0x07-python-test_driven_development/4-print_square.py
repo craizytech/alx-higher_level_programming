500721a79cfd1437659275c2d97934a581275389
craizytech <craizytech@gmail.com>
2024-07-08 10:49:18 +0300

    print_square
    This method prints a square to the stdout using # and along with it is the tests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module prints a square."""
+
+
+def print_square(size):
+    """
+    This function prints a square using the characters '#'
+
+    Args:
+        size (int): this is the size of the square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for i in range(size):
+        print("#" * size)
