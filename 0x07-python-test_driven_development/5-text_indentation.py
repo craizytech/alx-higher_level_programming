a3409fe1b657dfe06d46c914d14a6c06e3330adb
craizytech <craizytech@gmail.com>
2024-07-08 12:11:35 +0300

    text indentation
    This function prints text with blank lines

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module prints a sample test"""
+
+
+def text_indentation(text):
+    """
+    This function prints a text with 2 new lines after each of these
+    characters: .,?,:
+
+    Args:
+        text (str): The text to be printed
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    sentences = []
+    sentence = ""
+
+    for char in text:
+        if char in ['.','?',':']:
+            sentence += char
+            sentences.append(sentence.strip())
+            sentence = ""
+        else:
+            sentence += char
+    if sentence:
+        sentences.append(sentence.strip())
+
+    indented_text = "\n\n".join(sentences)
+    print(indented_text, end="")
