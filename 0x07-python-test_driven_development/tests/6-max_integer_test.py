7a4dd87da5ab00a995bdb5548e485cbf495073bb
craizytech <craizytech@gmail.com>
2024-07-09 13:58:26 +0300

    Added the method to check the maximum integer in a list, and also included unittests for this

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This is the test module for 6-max_integer_test.py"""
+
+
+import unittest
+max_integer = __import__('6-max_integer_test').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """This class contains the test methods for the max integer function"""
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+    
+    def test_list_with_int(self):
+        self.assertEqual(max_integer([1,2,4]), 4)
+    
+    def test_list_with_nonint(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, 'a'])
+
+if __name__ == "__main__":
+    unittest.main()
