eb52ac576c4adcec1846f915071a80c4bc1447ff
craizytech <craizytech@gmail.com>
2024-07-11 10:01:09 +0300

    pushed with some incomplete code

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines matrix multiplication"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    This function takes two matrices multiplies them together and returns
+    a resulting matrix.
+    Args:
+        m_a (list): this is the first matrix
+        m_b (list): this is the second matrix
+    Return (list): The resulting matrix after m_a * m_b
+    """
+
