60bfda0bce1f5cf5e9263d85efaa0b6ed5fa34f5
craizytech <craizytech@gmail.com>
2024-07-13 00:43:57 +0300

    added some tests for edge cases

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -13,17 +13,17 @@ def matrix_mul(m_a, m_b):
     """
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    
+
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    
+
     for row in m_a:
         if not isinstance(row, list):
             raise TypeError("m_a must be a list of lists")
 
         if len(row) != len(m_a[0]):
             raise TypeError("each row of m_a must be of the same size")
-        
+
         for _ in row:
             if not isinstance(_, (int, float)):
                 raise TypeError("m_a should contain only integers or floats")
@@ -38,17 +38,17 @@ def matrix_mul(m_a, m_b):
         for _ in row:
             if not isinstance(_, (int, float)):
                 raise TypeError("m_b should contain only integers or floats")
-            
+
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    
+
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    
-    m = len(m_a)         # Number of rows in the first matrix 
+
+    m = len(m_a)         # Number of rows in the first matrix
     n = len(m_a[0])      # Nuber of columns in m_a (no of rows in m_b)
     p = len(m_b[0])      # Number of columns in B
 
@@ -61,5 +61,5 @@ def matrix_mul(m_a, m_b):
             for k in range(n):
                 sum += m_a[i][k] * m_b[k][j]
             C[i][j] = sum
-    
+
     return C
