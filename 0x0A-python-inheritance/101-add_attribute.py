3ea2b799f505f97e79374ff09e95234cc1ca19fe
craizytech <craizytech@gmail.com>
2024-07-13 20:04:03 +0300

    wrote a function that adds a new attribute to an object if its possible otherwise it raises a TypeError

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This defines how attributes can be added to an object dynamicaly"""
+
+def add_attributes(obj, attr_name, attr_value):
+    """
+    Add a new attribute to an object if possible
+
+    Args:
+        obj: The object to which the attribute is to be added
+        attr_name: The name of the attribute
+        attr_value: The value of the attribute
+
+    Raises:
+        TypeError: If the object can't have new attributes
+    """
+    if hasattr(obj, "__dict__"):
+        setattr(obj, attr_name, attr_value)
+    else:
+        raise TypeError("can't add a new attribute")
