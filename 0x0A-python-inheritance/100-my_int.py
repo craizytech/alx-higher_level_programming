72384e4782ddf91ef9392e0da7c919c79acdb6f3
craizytech <craizytech@gmail.com>
2024-07-13 19:46:03 +0300

    wrote a class MyInt that inherits from int and alters the behavoir of the magic comparison methods

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module creates a class that inherits from int"""
+
+class MyInt(int):
+    """This class inherits from int and alters the magic methods"""
+
+    def __eq__(self, other):
+        """This magic method inverts the eq behaviour"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """This magic method inverts the ne behaviour"""
+        return super().__eq__(other)
