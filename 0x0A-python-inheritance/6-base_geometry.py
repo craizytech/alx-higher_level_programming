3815718130e09aa94b77753c94f371acc9a382c6
craizytech <craizytech@gmail.com>
2024-07-13 14:42:39 +0300

    Defines the class BaseGeometry with an area that isn't implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """This class defines the base geometry"""
+    def area(self):
+        """This method returns the area"""
+        raise Exception("area() is not implemented")
