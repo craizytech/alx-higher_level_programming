f7807049e237cb280e2fe42e08191bac9745da4b
craizytech <craizytech@gmail.com>
2024-07-13 17:22:38 +0300

    created a rectangle that inherits from the Base Class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module defines the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """This class defines the base geometry"""
+
+    def area(self):
+        """This method returns the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This method validates value"""
+
+        if not type(value) is int:
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """This module defines a rectangle that iherits from the base class"""
+
+    def __init__(self, width, height):
+        """This is the constructor of the rectangle."""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
