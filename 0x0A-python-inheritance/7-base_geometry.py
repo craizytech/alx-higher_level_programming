a8d666d72a7571b5db9c88d3a9c8720018018328
craizytech <craizytech@gmail.com>
2024-07-13 15:32:19 +0300

    Added tests for the BaseGeometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """This class defines the base geometry"""
+
+    def area(self):
+        """This method returns the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This method validates value"""
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
