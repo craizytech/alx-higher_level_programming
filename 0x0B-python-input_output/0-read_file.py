76674480a84942256a8de287cea4fb153bc64e39
craizytech <craizytech@gmail.com>
2024-07-14 08:54:22 +0300

    method that reads a text file and prints its contents to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/user/bin/python3
+"""This module reads a test file and prints it out to stdout"""
+
+
+def read_file(filename=""):
+    """Read the contents of the file and print it to the stdout
+    Args:
+        filename: the filename of the file to be read
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
