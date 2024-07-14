f2a7649839383db7f6690f7a9ed4803a0915cfde
craizytech <craizytech@gmail.com>
2024-07-14 09:04:20 +0300

    method that writes to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module writes to a file"""
+
+def write_file(filename="", text=""):
+    """
+    This method writes to a file.
+    Args:
+        filename: name of the file to be written on
+        text: the contents to write to the file
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        num = file.write(text)
+
+    return num
