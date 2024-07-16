e8b06cf1c5ba62c62d5f80ba9bb30cc0a12f5b26
craizytech <craizytech@gmail.com>
2024-07-16 17:14:32 +0300

    Added the getter and setter methods for all the instance attribbutes of the rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,47 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """This is the constuctor class"""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    
+    @property
+    def width(self):
+        """this is the width getter method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """This is the width setter method"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """This is the height getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """This is the height setter method"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """This is the x getter method"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """This is the setter for the x attribute"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """This is the y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """This is the y setter method"""
+        self.__y = value
